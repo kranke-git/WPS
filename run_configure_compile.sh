49d93e5ba8095ee7eeea197a176e36cc4d7416dd
kranke <pgiani@nd.edu>
2022-05-06 16:23:45 -0400

    Adding config and compile script

---
+++ run_configure_compile.sh
@@ -0,0 +1,11 @@
+module load mvapich2/2.3.1/intel/19.0
+ 
+export NETCDF='/afs/crc/group/EnvStat/build'
+export JASPERINC='/afs/crc/group/EnvStat/build/include'
+export JASPERLIB='/afs/crc/group/EnvStat/build/lib'
+export LIBTIFF='/afs/crc/group/EnvStat/build/lib'
+export GEOTIFF='/afs/crc/group/EnvStat/build/lib'
+export WRF_DIR='/afs/crc/group/EnvStat/WRF-src/WRF-4.3.3'
+#./configure
+echo 17 | ./configure
+./compile
