24dbcff819e47ce4d30bb80704d88b2aab555219
kranke <pgiani@nd.edu>
2022-05-13 00:50:34 -0400

    Modified config and compile script

--- run_configure_compile.sh
+++ run_configure_compile.sh
@@ -5,7 +5,7 @@ export JASPERINC='/afs/crc/group/EnvStat/build/include'
 export JASPERLIB='/afs/crc/group/EnvStat/build/lib'
 export LIBTIFF='/afs/crc/group/EnvStat/build/lib'
 export GEOTIFF='/afs/crc/group/EnvStat/build/lib'
-export WRF_DIR='/afs/crc/group/EnvStat/WRF-src/WRF-4.3.3'
+export WRF_DIR='/afs/crc/group/EnvStat/WRF-src/WRF'
 #./configure
 echo 17 | ./configure  #Serial Intel
 ./compile
