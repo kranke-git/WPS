3f8cb77e7e42e7e314fc54e2f5fa60c48ac57334
kranke <pgiani@nd.edu>
2022-06-06 18:49:06 -0400

    Changed ec_rec_len in g1print for ECMWF, see here full description: https://forum.mmm.ucar.edu/phpBB3/viewtopic.php?f=32&t=433&p=1279&hilit=ec_rec_len#p1279

--- run_configure_compile.sh
+++ run_configure_compile.sh
@@ -1,11 +1,9 @@
-module load mvapich2/2.3.1/intel/19.0
- 
 export NETCDF='/afs/crc/group/EnvStat/build'
 export JASPERINC='/afs/crc/group/EnvStat/build/include'
 export JASPERLIB='/afs/crc/group/EnvStat/build/lib'
 export LIBTIFF='/afs/crc/group/EnvStat/build/lib'
 export GEOTIFF='/afs/crc/group/EnvStat/build/lib'
-export WRF_DIR='/afs/crc/group/EnvStat/WRF-src/WRF'
+export WRF_DIR='/afs/crc/group/EnvStat/src/WRF/'
 #./configure
 echo 17 | ./configure  #Serial Intel
 ./compile
